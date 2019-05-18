9c01f7ba50bf40d321c3717bcd644685e9f7578f
Sanqui <me@sanqui.net>
2019-05-18 17:55:17 +0200

    Initial commit

---
+++ cedulky.py
@@ -0,0 +1,24 @@
+import os
+import yaml
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    consoles = []
+    for filename in os.listdir("consoles"):
+        consoles.append(filename.split(".")[0])
+    return render_template("index.html",
+        consoles=consoles
+    )
+
+@app.route("/<console>")
+def page(console):
+    with open(f"consoles/{console}.yaml") as f:
+        parameters = yaml.safe_load(f)
+    return render_template("RH_Layout.html",
+        **parameters
+    )
+
+app.run(debug=True)
