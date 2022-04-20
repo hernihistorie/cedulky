c10b37ee5be5b227991ceef90b879c9d0d76199c
Sanqui <me@sanqui.net>
2022-04-20 11:18:18 +0200

    Add Markdown

--- cedulky.py
+++ cedulky.py
@@ -1,8 +1,16 @@
+import re
 import os
 import yaml
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, Markup
+from flaskext.markdown import Markdown
 
+app = Flask(__name__)
+markdown = Markdown(app)
+
+@app.template_filter('fix_spaces')
+def fix_spaces(text):
+    text = re.sub("(\d) ", "\\1\u00a0", text)
+    return text
 
 @app.route("/")
 def index():
@@ -10,17 +18,18 @@ def index():
     for filename in os.listdir("consoles"):
         consoles.append(filename.split(".")[0])
     return render_template("index.html",
-                           consoles=consoles
-                           )
-
+        consoles=consoles
+    )
 
 @app.route("/<console>")
 def page(console):
     with open(f"consoles/{console}.yaml", encoding="utf-8") as f:
         parameters = yaml.safe_load(f)
+    
+
     return render_template("RH_Layout.html",
-                           **parameters
-                           )
+        **parameters
+    )
 
 
-app.run(debug=True)
+app.run(port=8099, debug=True)
